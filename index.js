c864799701f6dc54cace6784559d0fd216e88197
JustinpThomasson <Foreveraendeavour@gmail.com>
2024-10-09 09:30:19 -0400

    brute forced

--- index.js
+++ index.js
@@ -1,2 +1,49 @@
 
 // Write your code here
+
+const num1 = 1;
+const num2 = 62;
+const multiply = num1 * num2; 
+// const multiply2 = () => num1 * num2;
+// function multiply() {
+//     return num1 * num2;
+// }
+
+// console.log('multiply:', multiply);
+
+// console.log('multiply2: ', multiply2());
+
+// const random
+
+// const random2 = Math.random() * 10; // === 88.00585 (sometimes)
+
+// const random= Math.floor(random2) // 88.005585 => 88
+
+let random = Math.random() * 10; // === 88.00585 (sometimes)
+
+ random= Math.floor(random) // 88.005585 => 88
+
+ console.log(random)
+
+
+// mod equation that calcs a remainder equal to 4
+//Declaring num3 and num4
+const num3 = 4;
+const num4 = 100;
+const mod = num3 % num4
+
+// max will return 20 as the highest number in the set
+const max = Math.max (20)
+// const myFunc = (foo) => {
+//     console.log('hello world')
+//     // return 'hello world';
+// }
+
+// function yoFunc(foo) {
+//     return 'hello world';
+// }
+// console.log(myFunc())
+// myOther()
+// const multiply = ( ) => { 
+//     return num1*num2
+// } 
